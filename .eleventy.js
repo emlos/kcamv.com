52485927f65450a015a74a13012ff47bf70b6a84
[milo] <slepowronska02@gmail.com>
2023-08-28 13:11:29 +0200

    but i forgot to commit earlier oh well. in the middle of integratimg automatically generated sizes of images for the gallery into the workflow
    ok that w0rked! code for colelction of images implemented
    disregard that stupid sizing stuff, were gonna do it differently
    anyway oh Deafault assets not included
    
    TYLE BYLO SIE PIERDOLENIA. a tu masz
    um. gallery done, no dummy data included

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,10 @@ const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 const markdownIt = require("./src/markdown.js");
 const outdent = require("outdent");
 const path = require("node:path");
+const glob = require("glob-promise");
 
-const OUTPUTDIR = "public"
-
+const OUTPUTDIR = "public";
+const GALLERY_SIZE = 600;
 
 
 async function imageShortcode(src, alt, sizes) {
@@ -39,19 +40,33 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/scripts/*.js");
 
-  
-  eleventyConfig.addShortcode("date", () => `${new Date().getUTCDate}`);
-  
+  //collections [code is only allowed with GOTHIC comments! boo!]
+
+  // 1. to filter images for the gallery and add them to a collection for easier iteration :skull:
+  eleventyConfig.addCollection("artexamples", async (collectionApi) => {
+    const base = "./src/images/examples/";
+    let images = await glob(base + "*.{png,jpg,jpeg}");
+    
+    //actual collection to be born into eleventy proper
+    let collection = images.map((i) => {
+      return {
+        url: i.split("./src")[1], // "/images/examples/xxxxxxx.png", removes the ./src lol
+        title: "test"
+      };
+    });
+
+    return collection;
+  });
+
+  //shortcodes
   eleventyConfig.addPairedShortcode("tag", markdownToHtmlShortcode);
   eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
 
   //filters
 
-
   //plugins
-  eleventyConfig.addPlugin(faviconsPlugin, {"outputDir": OUTPUTDIR});
+  eleventyConfig.addPlugin(faviconsPlugin, { outputDir: OUTPUTDIR });
   eleventyConfig.setLibrary("md", markdownIt);
-
 
   return {
     dir: {
@@ -61,3 +76,4 @@ module.exports = function (eleventyConfig) {
     },
   };
 };
+
